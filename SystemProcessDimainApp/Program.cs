298c5a0cca16b5313c1f89b4c1019a94b0c4158d
maxefimov <efimov.top.academy@gmail.com>
2023-04-29 16:59:17 +0300

    Добавьте файлы проекта.

---
+++ SystemProcessDimainApp/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using System.Reflection;
+
+AppDomain domain = AppDomain.CurrentDomain;
+Console.WriteLine($"Name:\t{domain.FriendlyName}");
+Console.WriteLine($"Base Dir:\t{domain.BaseDirectory}");
+Console.WriteLine();
+
+Assembly[] assemblies = domain.GetAssemblies();
+foreach (Assembly assembly in assemblies)
+    Console.WriteLine(assembly.GetName().Name);
